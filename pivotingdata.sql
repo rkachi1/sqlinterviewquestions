55970a030f995f52cbc09d6fda985d46a1763d38
rkachi1 <rangarajkk@gmail.com>
2022-08-28 19:28:25 -0500

    added initial code

--- pivotingdata.sql
+++ pivotingdata.sql
@@ -8,7 +8,26 @@ CREATE TABLE PIVOTTABLE(
 insert into pivottable values ('name','adam',1)
 insert into pivottable values ('gender','male',1)
 insert into pivottable values ('salary','50000',1)
-
+-- table output looks like
+-- id | Name | gender | salary
+-- 1  | adam | male   | 50000
 -- two ways of converting from columns to rows also known as pivoting
 
 -- Solution 1 - Using Case statements
+-- Max function is used to avoid empty or blank cells
+select id, 
+MAX(case when Name = 'name' then value else '' end) as name,
+MAX(case when Name = 'gender' then value else '' end) as gender,
+MAX(case when Name = 'salary' then value else '' end) as salary
+from pivottable
+GROUP BY ID
+
+-- Solution 2- using Pivot function
+
+select [id],[Name],[gender],[salary]
+from 
+(select id,name as Ename ,value
+from pivottable) as source_table
+PIVOT(
+MAX(VALUE)
+FOR ENAME IN ([Name],[gender],[salary])) as finaltable
